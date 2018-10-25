aea809a4e4198cc1fcbe76d18c4ac0ced0f88de8
Tom Lee <thomas.j.lee@gmail.com>
2018-10-25 14:10:55 -0400

    revised fx

--- python/fx/scroll.lua
+++ python/fx/scroll.lua
@@ -2,18 +2,17 @@ print("running scroll.lua")
 
 tmr.unregister(6)
 ws2812_effects.stop()
-DELAY = 50
+DELAY = 200
 for j=1,75 do
-        input_b = (j - 1) * 3
-	g, r, b = color_utils.hsv2grb(255, 255, input_b)
-	strip_buffer:set(j, g, r, b)
-	strip_buffer:set(151-j, g, r, b)
+    input_b = (j - 1) * 3
+    g, r, b = color_utils.hsv2grb(255, 255, input_b)
+    strip_buffer:set(j, g, r, b)
+    strip_buffer:set(151-j, g, r, b)
 end
 
 function tic()
-	ws2812.write(strip_buffer)
-	strip_buffer:shift(1, ws2812.SHIFT_CIRCULAR)
-	tmr.alarm(6, DELAY, tmr.ALARM_SINGLE, tic)
+    ws2812.write(strip_buffer)
+    strip_buffer:shift(1, ws2812.SHIFT_CIRCULAR)
 end
 
-tmr.alarm(6, DELAY, tmr.ALARM_SINGLE, tic)
+tmr.alarm(6, DELAY, tmr.ALARM_AUTO, tic)
--- python/fx/scroll_middle.lua
+++ python/fx/scroll_middle.lua
@@ -3,21 +3,21 @@ print("running scroll_middle.lua")
 tmr.unregister(6)
 ws2812_effects.stop()
 
-DELAY = 50
+DELAY = 200
 
 buf_a = ws2812.newBuffer(75, 3)
 buf_b = ws2812.newBuffer(75, 3)
-for i=1,75,1 do
-	g, r, b = color_utils.hsv2grb(255, 255, (i-1) * 3)
-	buf_a:set(i, g, r, b)
-	buf_b:set(76-i, g, r, b)
+for j=1,75 do
+    input_b = (j - 1) * 3
+    g, r, b = color_utils.hsv2grb(255, 255, input_b)
+    buf_a:set(j, g, r, b)
+    buf_b:set(76-j, g, r, b)
 end
 
 function tic()
-	ws2812.write(buf_a .. buf_b)
-	buf_a:shift(-1, ws2812.SHIFT_CIRCULAR)
-	buf_b:shift(1, ws2812.SHIFT_CIRCULAR)
-	tmr.alarm(6, DELAY, tmr.ALARM_SINGLE, tic)
+    ws2812.write(buf_a .. buf_b)
+    buf_a:shift(-1, ws2812.SHIFT_CIRCULAR)
+    buf_b:shift(1, ws2812.SHIFT_CIRCULAR)
 end
 
-tmr.alarm(6, DELAY, tmr.ALARM_SINGLE, tic)
+tmr.alarm(6, DELAY, tmr.ALARM_AUTO, tic)
