de2da6f32ab24a5c19e0f76e6a86d44900d35287
Tom Lee <thomas.j.lee@gmail.com>
2018-10-25 14:26:55 -0400

    echo message

--- nodemcu/application.lua
+++ nodemcu/application.lua
@@ -5,20 +5,21 @@ function connect()
     ws = websocket.createClient()
     
     ws:on("connection", function(ws)
-	sec, usec, rate = rtctime.get()
+        sec, usec, rate = rtctime.get()
         print(string.format("connected to %s at %s", SERVER, sec))
-	ws:send(wifi.sta.getmac())
+        ws:send(wifi.sta.getmac())
     end)
     
     ws:on("receive", function(_, msg, opcode)
-	   print("received payload")
+       print("received payload")
+       print(msg)
        pcall(loadstring(msg))
     end)
     
     ws:on("close", function(_, status)
-	   print("connection closed")
-	   ws = nil
-	   tmr.alarm(6, 1000, tmr.ALARM_SINGLE, connect)
+       print("connection closed")
+       ws = nil
+       tmr.alarm(6, 1000, tmr.ALARM_SINGLE, connect)
     end)
 
     print(string.format("connecting to %s:8765", SERVER))
